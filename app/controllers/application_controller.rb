43c66c18c1763fcbaa142fe7c4c62da7336a4a2b
xttebrocx <samueldcorbett@uchicago.edu>
2022-04-22 19:10:57 +0000

    Headers complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
 
+def addition
+  render(:template => "/mathematics_form/addition_form.html.erb")
+end
+
+def subtraction
+  render(:template =>"/mathematics_form/subtraction_form.html.erb")
+end
+
+def multiplication
+  render(:template => "/mathematics_form/multiplication_form.html.erb")
+end
+
+def division
+  render(:template => "/mathematics_form/division_form.html.erb")
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
 
+
+
+  get("/add", :controller=>"application", :action => "addition")
+
+  get("/subtract", :controller=>"application", :action => "subtraction")
+
+  get("/multiply", :controller=>"application", :action => "multiplication")
+  
+  get("/divide", :controller=>"application", :action => "division")
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
